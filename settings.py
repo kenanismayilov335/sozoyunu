a9e4b917aa87c1f5eeb98d0171b05db9d095c131
kenanismayilov335 <74872919+kenanismayilov335@users.noreply.github.com>
2021-08-29 22:28:25 +0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-TOKEN = "1977082924:AAEcETY5eMNd6rt_mR-LZSIxRtezIHBP6NA"
+TOKEN = "1785535365:AAHHMkoeGGDlFt4ELInkk8oeLjL2PVig3NI"
 with open('sozler.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
