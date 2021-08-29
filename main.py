2886728b8888557722efaee61468ff52c2a33baf
kenanismayilov335 <74872919+kenanismayilov335@users.noreply.github.com>
2021-08-29 23:28:30 +0400

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def command_start(update, context: CallbackContext):
 
         keyboard = [[addme],[sohbet],[admin]]
         reply_markup = InlineKeyboardMarkup(keyboard)
-        update.message.reply_text(' Salam Mən Söz Oyun Botuyam Oynamaq Üçün Məni Qrupunuza Əlave Edin!', reply_to_message_id=True, reply_markup=reply_markup)
+        update.message.reply_text(' Salam Mən Söz Oyun Botuyam Oynamaq Üçün Məni Qrupunuza Əlave Edin! Kömek Üçün /komek@SozOyunuFarzBot Komutunu İşledin', reply_to_message_id=True, reply_markup=reply_markup)
     else:
         chat_id = update.message.chat.id
         user_id = update.message.from_user.id
